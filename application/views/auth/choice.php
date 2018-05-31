0df4a367c5b5fdc409fb131d192a6e358877279b
ashvin.ebiztrait@gmail.com <ashvin.ebiztrait@gmail.com>
2018-05-31 11:48:01 +0530

    codeigniter project

---
+++ application/views/auth/choice.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<h1>Your choice</h1>
+
+<p><a href="<?php echo site_url('index.php/'); ?>">Home</a></p>
+
+<p><a href="<?php echo site_url('index.php/admin'); ?>">Admin</a></p>
+
+<p><a href="<?php echo site_url('index.php/auth/logout'); ?>">Logout</a></p>
+
